86cb3ac6f128255457b992f7eef5ca959fe4380d
mithilarai <74225672+mithilarai@users.noreply.github.com>
2022-05-23 15:56:41 +0530

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -4,27 +4,25 @@ import plotly.figure_factory as ff
 import statistics
 import pandas as pd 
 import random
+
+
 df = pd.read_csv('c110.csv')
 data=df['temp'].tolist()
-import time 
 
-
- 
 population_mean=statistics.mean(data)
 sdp=statistics.stdev(data)
 print('population sd -----> ',sdp)
 print('population MEAN -----> ',population_mean)
-sample=[]
 
 
 
 
 def samp_mean():
+    sample=[]
+
     for i in range(0,100):
         temp=data[random.randint(0,len(data)-1)]
         sample.append(temp)
-    
-    
     mean_of_sample=statistics.mean(sample)
     return (mean_of_sample)
  
@@ -35,10 +33,7 @@ def cal_mean():
     for i in range(0,1000):
         means=samp_mean()
         mean_list.append(means)
-    
-    
-    
-   
+
     print(' sampiling sd -----> ',statistics.stdev(mean_list))
     print('MEAN OF sampiling distribution',statistics.mean(mean_list))
     # fig2=ff.create_distplot([mean_list],['ali'],show_hist=False)
